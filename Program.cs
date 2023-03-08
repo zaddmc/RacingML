468597a6a4ef09396e3e2c187e563ef1694b7b8a
Zadd ZC <davidbk927@gmail.com>
2023-03-08 09:47:59 +0100

    switch introduced

--- Program.cs
+++ Program.cs
@@ -14,7 +14,42 @@ namespace RacingML
             sr.ReadLine();
 
 
-            DrawFrame(MakeFrame(sr).frame);
+
+            bool isActive = true;
+            while (isActive)
+            {
+                string[] command = Console.ReadLine().ToLower().Split(' ');
+                switch (command[0])
+                {
+                        // if the input is not something that have been implementet yet it will just give a failure message and give another try
+                    default:
+                        Console.WriteLine("'{0}' is not recognized as an command", command[0]);
+                        break;
+
+                        //if the user wishes to exit the program they can safely use this command
+                    case "exit":
+                        isActive = false;
+                        break;
+
+                        // the user can clear the command prompt
+                    case "clear":
+                        Console.Clear();
+                        break;
+
+                        // the user can draw as many frames as wanted, note that if there isnt specified an amount it will only draw 1 frame
+                    case "drawframe":
+                        int num = 1;
+                        if (command.Length > 1 && int.TryParse(command[1], out num))
+                            for (int i = 0; i < num; i++)
+                                DrawFrame(MakeFrame(sr).frame);
+                        else
+                            DrawFrame(MakeFrame(sr).frame);
+                        Console.WriteLine("-Succesfully drawn {0} Frame", num);
+                        break;
+                } // main switch
+            } // while opperational
+
+
 
 
 
@@ -28,7 +63,26 @@ namespace RacingML
 
 
             Console.WriteLine("End");
-        }
+        } // Main
+        static (int label, byte[][] frame) MakeFrame(StreamReader sr)
+        {
+            // the first thing is to initialzize the memory, i do this in a byte as its value is from 0 to 255 and that is perfect for my grayscaled input
+            // if i did not use byte the memory would be hit quite hard and i dont need more information than a byte anyways
+            byte[][] pixels = new byte[28][];
+            for (int i = 0; i < 28; i++)
+                pixels[i] = new byte[28];
+
+            // i read the next line in my training set and save it in a string array
+            string[] strings = sr.ReadLine().Split(',');
+
+            // i go through every value and position it in the byte array with an array inside
+            for (int i = 0; i < 28; i++)
+                for (int j = 0; j < 28; j++)
+                    pixels[j][i] = Convert.ToByte(strings[i * 28 + j + 1]);
+
+            // i return the the first value in the sequnce as it contains the label, the rest is given as the frame
+            return (Convert.ToInt32(strings[0]), pixels);
+        } // MakeFrame
         static void DrawFrame(byte[][] frame)
         {
             // if the program doesnt recognize the following commands then use this code in powershell : dotnet add package System.Drawing.Common --version 7.0.0
@@ -53,21 +107,6 @@ namespace RacingML
 
             // i save it to: E:\Stuff\Programming\Test\RacingML\bin\Debug\net7.0
             bmp.Save("test.jpg");
-        }
-        static (int label, byte[][] frame) MakeFrame(StreamReader sr)
-        {
-
-            byte[][] pixels = new byte[28][];
-            for (int i = 0; i < 28; i++)
-                pixels[i] = new byte[28];
-
-            string[] strings = sr.ReadLine().Split(',');
-
-            for (int i = 0; i < 28; i++)
-                for (int j = 0; j < 28; j++)
-                    pixels[j][i] = Convert.ToByte(strings[i * 28 + j + 1]);
-
-            return (Convert.ToInt32(strings[0]), pixels);
-        }
+        } // DrawFrame
     }
 }
